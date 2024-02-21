7b68f5fd72265d8821f1dd854e73b1b7137b0059
samuel kuhn <samuel.kuhn@gmx.ch>
2024-02-21 17:55:10 +0100

    added run and setup bash script

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [ $USER != root ]
+then
+  echo -e "Please make sure you run this as root!\nExiting!"
+  exit
+fi
+
 echo -e "pulling latest docker image...\n"
 docker image pull itzg/minecraft-server > /dev/null
+
+source env/bin/activate
+
 python3 app.py
--- setup.sh
+++ setup.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
+if [ $USER != root ]
+then
+  echo -e "Please make sure you run this as root!\nExiting!"
+  exit
+fi
 
-docker image pull itzg/minecraft-server
+if ! command -v docker >/dev/null  2>&1
+then
+  echo -e "Please make sure docker is installed!\nExiting!"
+  exit
+fi
+echo -e "pulling latest docker image...\n"
+docker image pull itzg/minecraft-server > /dev/null
+
+if ! command -v python3 >/dev/null  2>&1
+then
+  echo -e "Please make sure python3 is installed!\nExiting!"
+  exit
+fi
+
+if ! command -v pip >/dev/null  2>&1
+then
+  echo -e "Please make sure pip is installed!\nExiting!"
+  exit
+fi
+
+if ! command -v virtualenv >/dev/null  2>&1
+then
+  echo -e "installing virtualenv...\n"
+  apt update >/dev/null && apt install python3-virtualenv -y >/dev/null  2>&1
+fi
+#enabling virtualenv
+virtualenv env >/dev/null
+source env/bin/activate
+
+echo -e "installing all requirements...\n"
+pip install -r requirements.txt > /dev/null
+
+echo -e "Everything was set up successfully!"
